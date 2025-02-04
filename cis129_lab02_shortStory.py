dbe57346b6f09ff1090c1c2f7c451788cfd2b327
mdavis113 <Mdavis113@mail.pima.edu>
2025-02-03 18:30:20 -0700

    Update cis129_lab02_shortStory.py
    short story about a little code that  could

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,14 @@
-print ('Hello, World!')
+# Author: Maurice Davis
+# this program is a brief story about a little code and its importance to the whole program
+# removed hello world and prints a simple short story
+print ("This is a short story about a little code who could")
+
+print ('''Deep inside a bustling server, a tiny piece of code named loop watched as massive,
+
+programs ran important tasks. The others mocked loop for being too small to matter, but one day,
+
+the system froze, unable to process vital requests. While the bigger code scrambled, loop quietly repeated its simple command, 
+
+until help finally came. From then on, loop was no longer overlooked
+
+                  --he was little code that did''')
